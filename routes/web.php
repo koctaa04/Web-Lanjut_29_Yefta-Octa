7dd950a294597ae03f70619a1dea41a738fa73fd
koctaa04 <yeftaocta04@gmail.com>
2025-02-27 19:09:00 +0700

    Basic Routing Praktikum

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,79 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// BASIC ROUTE
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+// Route::get('/hello', function () {
+//     return 'Hello World';
+// });
+
+// Route::get('/', function () {
+//     return 'Selamat Datang';
+// });
+// Route::get('/about', function () {
+//     return 'Nim Saya : 2341720110';
+// });
+
+// // ROUTE PARAMETERS
+// // Route::get('/user/{name}', function ($name) {
+// //     return 'Nama saya ' . $name;
+// // });
+
+
+// Route::get('/posts/{post}/comments/{comment}', function 
+// ($postId, $commentId) {
+//     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+// });
+
+// Route::get('/articles/{id}', function ($id) {
+//     return 'id saya ' . $id;
+// });
+
+// // OPTIONAL PARAMETERS
+// // Route::get('/user/{name?}', function ($name=null) {
+// //     return 'Nama saya '.$name;
+// // });
+
+// Route::get('/user/{name?}', function ($name='John') {
+//     return 'Nama saya '.$name;
+// });
+
+// Route Group dan Route Prefixes
+Route::middleware(['first', 'second'])->group(function () {
+    Route::get('/', function () {
+    // Uses first & second middleware...
+    });
+   Route::get('/user/profile', function () {
+    // Uses first & second middleware...
+    });
+   });
+   Route::domain('{account}.example.com')->group(function () {
+    Route::get('user/{id}', function ($account, $id) {
+    //
+    });
+//    });
+//    Route::middleware('auth')->group(function () {
+//    Route::get('/user', [UserController::class, 'index']);
+//    Route::get('/post', [PostController::class, 'index']);
+//    Route::get('/event', [EventController::class, 'index']);
+//    });
+
+
+// Route Prefix
+Route::prefix('admin')->group(function () {
+    Route::get('/user', [UserController::class, 'index']);
+    Route::get('/post', [PostController::class, 'index']);
+    Route::get('/event', [EventController::class, 'index']);
+    });
+
+// Redirects routes
+Route::redirect('/here', '/there')
+
+// View routes
+Route::view('/welcome', 'welcome');
+Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
